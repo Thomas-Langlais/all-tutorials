23fa9a2c8d4bfc3a7c192d31bc539f4d32bc26b6
Thomas-Langlais <tlang072@uottawa.ca>
2021-02-16 10:03:08 -0500

    Added a simple Hero service to fetch the hero data. It's stubbed with an observable (aka: async code) object to mimic fetching from an http client. Using Services should be used for object responsibilities (SOLID principals) and can be scoped for different things, like it can be used for the entire app or only used for a specific module. A common case being the user management module.

--- angular/heroes-tutorial/src/app/heroes/heroes.component.ts
+++ angular/heroes-tutorial/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,12 +12,11 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = null;
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-    
-    this.heroes = HEROES;
-
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero) {
