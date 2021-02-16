fdded836b2edf750620e2dabfc9f5c2b168189d7
Thomas-Langlais <tlang072@uottawa.ca>
2021-02-16 10:46:05 -0500

    Added a dashboard and enabled routing to it and the hero list component

--- angular/heroes-tutorial/src/app/app-routing.module.ts
+++ angular/heroes-tutorial/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // https://angular.io/tutorial/toh-pt5#add-the-approutingmodule
 // best practice is to separate the routing into a separate module.
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
