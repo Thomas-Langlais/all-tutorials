602dc9b45bc8f8848da4ee6bdb1afc18330d22e1
Thomas-Langlais <tlang072@uottawa.ca>
2021-02-15 20:58:34 -0500

    Used the angular directive *ngFor to iterate over a list of objects and display them. Used the [class.*]="condition" to set the stylings conditionally Used the *ngIf directive to render the a hero when it's not null. Used the event binding notation (eventName)="theComponentEventFunction(...args)"

--- angular/heroes-tutorial/src/app/heroes/heroes.component.ts
+++ angular/heroes-tutorial/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +9,19 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = null;
+  heroes: Hero[] = null;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
     
-    this.hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+    this.heroes = HEROES;
 
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 
 }
